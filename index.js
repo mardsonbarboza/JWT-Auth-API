e2b9987e4ccc0cf810901d57187322c656f63ca3
mardsonbarboza <mardson49@gmail.com>
2024-12-05 14:48:42 -0300

    feat: configuração inicial do servidor com body-parser e rota padrão para /api

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const RoutersUse = require('./routers/UserRouter');
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
+app.use('/api',RoutersUse);
 
 app.listen(8080,()=>{
     console.log('Server devidamente ligado');
