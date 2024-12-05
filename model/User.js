4c28de55d43ff85a77700398313859a7d4eeb190
mardsonbarboza <mardson49@gmail.com>
2024-12-05 14:49:24 -0300

    feat: implementação dos métodos findByEmail e create no modelo User

--- model/User.js
+++ model/User.js
@@ -1,7 +1,15 @@
-const knex = require('../database/Connection')
+const knex = require('../database/Connection');
 
-class User{
-    
+class User {
+    async findByEmail(email) {
+        const result = await knex.select('*').where({ email }).table('users').first(); 
+        return result || null; // Retorna o registro ou null
+    }
+
+    async create(name, email, password) {
+        await knex.insert({ name, email, password }).table('users');
+        return { status: true };
+    }
 }
 
 module.exports = new User();
