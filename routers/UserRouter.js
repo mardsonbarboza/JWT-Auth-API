2cbf9d5c2f05e0c2a321c0488b4285f9f52f6a40
mardsonbarboza <mardson49@gmail.com>
2024-12-05 14:49:52 -0300

    feat: criação da rota POST /user com validação de dados

---
+++ routers/UserRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controller/UserController');
+const {check} = require('express-validator')
+router.post('/user',[
+    check('name', 'O nome é obrigatório').notEmpty().trim().escape(),
+    check('email', 'O email é obrigatório e deve ser válido').isEmail().normalizeEmail(),
+    check('password', 'A senha deve ter no mínimo 6 caracteres').isLength({ min: 6 })
+],UserController.register);
+
+module.exports = router;
