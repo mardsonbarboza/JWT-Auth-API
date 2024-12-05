48935c03799ea05e38de3871858cb73af992e237
mardsonbarboza <mardson49@gmail.com>
2024-12-05 10:35:29 -0300

    chore: configurar dotenv

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+require('dotenv').config();
+
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
 
 app.listen(8080,()=>{
     console.log('Server devidamente ligado');
