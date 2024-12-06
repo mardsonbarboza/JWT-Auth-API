adf904bf6ae92495a82eb46abf119fdd3c8a0dc9
mardsonbarboza <mardson49@gmail.com>
2024-12-06 10:38:20 -0300

    feat: implementar findById, updateIsVerifield e atualizar create

--- model/User.js
+++ model/User.js
@@ -3,12 +3,22 @@ const knex = require('../database/Connection');
 class User {
     async findByEmail(email) {
         const result = await knex.select('*').where({ email }).table('users').first(); 
-        return result || null; // Retorna o registro ou null
+        return result || null; // Retorna o primeiro resultado ou null se não existir
     }
+    
 
     async create(name, email, password) {
         await knex.insert({ name, email, password }).table('users');
-        return { status: true };
+        return {result: await this.findByEmail(email), status : true};
+        
+    }
+    async findById(id) {
+        console.log("ID recebido no findById:", id); // Log para depuração
+        return await knex.select('*').where({ id }).table('users').first();
+    }
+    
+    async updateIsVerifield(id){
+        return await knex.update({isVeryfield:1}).where({id}).table('users') 
     }
 }
 
