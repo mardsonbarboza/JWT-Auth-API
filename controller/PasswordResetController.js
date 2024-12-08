810fb56e20b2d22d1837bbd820115150b6471685
mardsonbarboza <mardson49@gmail.com>
2024-12-08 20:04:05 -0300

    feat: implementar setup inicial de PasswordReset controller, model e router

---
+++ controller/PasswordResetController.js
@@ -0,0 +1,9 @@
+const User = require('../model/User');
+const moment = require('moment');
+
+
+class PasswordResetController{
+
+}
+
+module.exports = new PasswordResetController()
---
+++ model/PasswordReset.js
@@ -0,0 +1,7 @@
+const knex = require('../database/Connection');
+const nodeMailer = require('nodemailer');
+class PasswordReset{
+
+}
+
+module.exports = new PasswordReset();
