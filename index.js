f134e5daaddb69c9ab05b8d16e08eac1c5082b48
mardsonbarboza <mardson49@gmail.com>
2024-12-08 19:42:33 -0300

    chore: configurar cookie-parser

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
 const RoutersUse = require('./routers/UserRouter');
-
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
