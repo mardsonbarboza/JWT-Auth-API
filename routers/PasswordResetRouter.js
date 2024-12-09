b118f9abe2a1f00bb4502d18dcb5502e1d5885a9
mardsonbarboza <mardson49@gmail.com>
2024-12-08 23:35:43 -0300

    feat: implementar rotas /request post e /reset post

--- routers/PasswordResetRouter.js
+++ routers/PasswordResetRouter.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router()
+const PasswordResetController = require('../controller/PasswordResetController');
 
+// Endpoint para solicitar recuperação de senha
+router.post('/request', PasswordResetController.requestPasswordReset);
+
+// Endpoint para redefinir a senha
+router.post('/reset', PasswordResetController.resetPassword);
 module.exports = router;
