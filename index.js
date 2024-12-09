6350901ed795b95a5443541b5ecdf267e10032e8
mardsonbarboza <mardson49@gmail.com>
2024-12-08 23:34:38 -0300

    chore: configurar rota passwordreset

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
 const RoutersUse = require('./routers/UserRouter');
+const RoutersPasswordReset = require('./routers/PasswordResetRouter');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use('/api',RoutersUse);
+app.use('/api',RoutersPasswordReset)
 
 app.listen(8080,()=>{
     console.log('Server devidamente ligado');
