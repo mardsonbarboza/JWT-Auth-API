1ad1c984461845e93c564a961708c79466d6ee5b
mardsonbarboza <mardson49@gmail.com>
2024-12-09 00:03:35 -0300

    doc: implementar documentação

--- routers/PasswordResetRouter.js
+++ routers/PasswordResetRouter.js
@@ -2,9 +2,57 @@ const express = require('express');
 const router = express.Router()
 const PasswordResetController = require('../controller/PasswordResetController');
 
-// Endpoint para solicitar recuperação de senha
+/**
+ * @swagger
+ * /request:
+ *   post:
+ *     summary: Solicitar recuperação de senha
+ *     tags: [Password Reset]
+ *     requestBody:
+ *       description: Enviar o e-mail para redefinição
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "user@example.com"
+ *     responses:
+ *       200:
+ *         description: Sucesso na solicitação de redefinição de senha
+ *       400:
+ *         description: E-mail inválido ou não encontrado
+ */
 router.post('/request', PasswordResetController.requestPasswordReset);
 
-// Endpoint para redefinir a senha
+/**
+ * @swagger
+ * /reset:
+ *   post:
+ *     summary: Redefinir a senha do usuário
+ *     tags: [Password Reset]
+ *     requestBody:
+ *       description: Enviar o token e a nova senha para redefinir
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+ *               newPassword:
+ *                 type: string
+ *                 example: "novaSenha123"
+ *     responses:
+ *       200:
+ *         description: Senha redefinida com sucesso
+ *       400:
+ *         description: Token inválido ou expirado, ou erro ao redefinir a senha
+ */
 router.post('/reset', PasswordResetController.resetPassword);
+
 module.exports = router;
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,14 +1,101 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controller/UserController');
-const {check} = require('express-validator')
-router.post('/user',[
+const { check } = require('express-validator');
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: Operações relacionadas ao usuário.
+ */
+
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Registra um novo usuário
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome do usuário
+ *               email:
+ *                 type: string
+ *                 description: E-mail do usuário
+ *               password:
+ *                 type: string
+ *                 description: Senha do usuário
+ *     responses:
+ *       200:
+ *         description: Usuário registrado com sucesso
+ *       400:
+ *         description: Erro de validação
+ */
+
+/**
+ * @swagger
+ * /confirm/{token}:
+ *   get:
+ *     summary: Confirmação de e-mail do usuário
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         description: Token de confirmação do e-mail
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: E-mail confirmado com sucesso
+ *       400:
+ *         description: Token inválido
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login do usuário
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: E-mail do usuário
+ *               password:
+ *                 type: string
+ *                 description: Senha do usuário
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso
+ *       400:
+ *         description: E-mail ou senha inválidos
+ */
+
+router.post('/user', [
     check('name', 'O nome é obrigatório').notEmpty().trim().escape(),
     check('email', 'O email é obrigatório e deve ser válido').isEmail().normalizeEmail(),
     check('password', 'A senha deve ter no mínimo 6 caracteres').isLength({ min: 6 })
-],UserController.register);
+], UserController.register);
+
 router.get('/confirm/:token', UserController.confirmToken);
-router.post('/login',[
+
+router.post('/login', [
     check('email', 'E-mail inválido').optional().isEmail().normalizeEmail(),
-    UserController.login])
+    UserController.login
+]);
+
 module.exports = router;
